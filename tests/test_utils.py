4e53ad08bfec8729a0e3e755b8bb877bf69433e2
Helen <helen.catanese@addium.io>
2023-11-02 19:18:38 -0700

    Cleanup

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,12 +15,12 @@ from time import sleep
     ("USA, Idaho, St Maries", (47.3143542, -116.562667)),
     ("Baker County, Oregon, United States", (44.7259637, -117.6204818)),
     ("U. S. A., Kansas", (38.27312, -98.5821872)),
-    ({"country":"United States", "state":"Washington", 'subdivision': ''},(47.2868352, -120.212613)),
-    ({'country': 'South Africa', 'state': 'KwaZulu-Natal', 'subdivision': ''}, (-28.503833, 30.8875009)),
-    ({'city': 'platte', 'state': 'South Dakota', 'country': 'USA'}, (43.38694, -98.84453)),
+    ({"country":"United States", "state":"Washington", "subdivision": ""},(47.2868352, -120.212613)),
+    ({"country": "South Africa", "state": "KwaZulu-Natal", "subdivision": ""}, (-28.503833, 30.8875009)),
+    ({"city": "platte", "state": "South Dakota", "country": "USA"}, (43.38694, -98.84453)),
     ("Platte, South Dakota, United States", (43.38694, -98.84453)),
     ("Batangas, Calabarzon, Philippines", (13.9146826, 121.0867566)),
-    ("Batangas, Philippines", (13.9146826, 121.0867566)),
+    ("Philippines, Batangas", (13.9146826, 121.0867566)),
 
 ])
 def test_get_coords_from_location_name(location_name, expected):
@@ -29,11 +29,11 @@ def test_get_coords_from_location_name(location_name, expected):
     assert get_coords_from_location_name(location_name) == expected
 
 @pytest.mark.parametrize("latitude,longitude,expected", [
-    (46.7909974, -117.250452, {'city': 'Albion', 'province': 'Washington', 'country': 'United States'}),
-    (45.9896587, -94.6113288, {'city': 'Morrison County', 'country': 'United States', 'province': 'Minnesota'}),
-    (45.9896597, -94.6113288, {'city': 'Morrison County', 'country': 'United States', 'province': 'Minnesota'}),
-    (-33.928992, 18.417396, {'city': 'Cape Town', 'country': 'South Africa', 'province': 'Western Cape'}),
-    (32.937094, 35.083828, {'city': 'Acre', 'province': 'North District', 'country': 'Israel'}), # Ensure locations code in English
+    (46.7909974, -117.250452, {"city": "Albion", "province": "Washington", "country": "United States"}),
+    (45.9896587, -94.6113288, {"city": "Morrison County", "country": "United States", "province": "Minnesota"}),
+    (45.9896597, -94.6113288, {"city": "Morrison County", "country": "United States", "province": "Minnesota"}),
+    (-33.928992, 18.417396, {"city": "Cape Town", "country": "South Africa", "province": "Western Cape"}),
+    (32.937094, 35.083828, {"city": "Acre", "province": "North District", "country": "Israel"}), # Ensure locations code in English
 ])
 def test_get_location_name_from_coords(latitude, longitude, expected):
     sleep(0.3)
